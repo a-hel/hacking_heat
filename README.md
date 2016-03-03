4b6929e6b8506f50dabd5daa274812cd208d1223
Andreas Helfenstein (Work) <andreas.helfenstein@helsinki.fi>
2016-03-03 16:00:02 +0200

    img retrieval fixed

--- README.md
+++ README.md
@@ -3,3 +3,8 @@ __HACKING HEAT__
 Alternative title: Ignition Recognition
 
 Image recoginition workflow for identifying ovens from livingroom photos.
+
+Setting up of Google search engine:
+- Custom Search
+- Activate 'Image Search'
+- Sites to search: Search the entire web but emphasize included sites
--- main.py
+++ main.py
@@ -1,16 +1,57 @@
 """Main workflow for 'hacking heat'"""
 
-import numpy as np
-import matlab
 
-def img_stream(tag):
+import urllib2
+import io
+from apiclient.discovery import build
+from PIL import Image
+
+import numpy as np
+
+try:
+    import matlab
+except ImportError:
+    msg = """Matlab engine not installed: Visit
+
+>> http://se.mathworks.com/help/matlab/matlab_external/install-the-matlab-engine-for-python.html
+
+for more information."""
+    print msg
+
+def img_stream(tag, google_key="AIzaSyDckTUqtzDdKxKJqeat3HvPFwceAUVRbew", cx='009248554659861815589:hvztsnepnuc', startIndex=0):
     """Search google images for 'tag'.
     Arguments:
     tag (str): Google images search term
+    startIndex (int, optional): First image to retrieve
 
     Returns: The next image
     """
-    yield tag
+
+    service = build("customsearch", "v1",
+               developerKey=google_key)
+
+    res = service.cse().list(
+        q=tag,
+        cx=cx,
+        searchType='image',
+        num=3,
+        #imgType='clipart',
+        #fileType='png',
+        safe= 'off'
+    ).execute()
+
+    if not 'items' in res:
+        print 'No result !!\nres is: {}'.format(res)
+    else:
+        for item in res['items']:
+            #print('{}:\n\t{}'.format(item['title'], item['link']))
+            url = item['link']
+            imgRequest = urllib2.Request(url)#, headers=headers)
+            imgData = urllib2.urlopen(imgRequest).read()
+            image_file = io.BytesIO(imgData)
+            greyscale_img = Image.open(image_file).convert('L')
+            numeric_img = np.asarray(greyscale_img)
+            yield numeric_img
 
 def feature_extraction(img, feat_type="HOG"):
     """Extract the features from an image.
@@ -23,7 +64,30 @@ def feature_extraction(img, feat_type="HOG"):
 
     return img
 
-def matlab():
+def train_network(network, tags, feat_type="HOG", n_img=500, iterations=None):
+    """Train the network with images.
+    Arguments:
+    network (pybrain.): The network to be trained
+    tags (list): List of labels
+    feat_type (str): Features used for feature extraction
+    n_img (int, optional): Number of images used for training
+    iterations (int, optional): Max number of training cycles. If omitted, will
+        train until convergence.
+
+    Returns: True after successful training
+
+    Example:
+    train_network(my_network, tags=['apples', 'oranges'], feat_type="HOG",
+        n_img=250, iterations=20000)
+    """
+
+def predict_img(network, img):
+    label = 'label'
+    return label
+
+    return True
+
+def matlab_eng():
     """Initialize MATLAB engine.
     Install matlab engine:
     http://se.mathworks.com/help/matlab/matlab_external/install-the-matlab-engine-for-python.html
@@ -33,5 +97,8 @@ def matlab():
     return eng
 
 if __name__ == "__main__":
-    eng = matlab()
-    print eng.isprime(37)
+    #eng = matlab_eng()
+    #print eng.isprime(37)
+    for i in img_stream('orange'):
+        print i.shape
+        break
